9827f36ad7803098ec1535bc0583b3c9c16368af
Palazzio <ricardo.palazzio@gmail.com>
2017-05-14 23:27:10 -0300

    -first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+import {AlertaService} from './alerta.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Testes Eworks Angular!';
+  foto: string = 'favicon.ico';
+  desenvolvimento: string [] = ['Angular 2', 'JavaScript', 'TypeScript', 'HTML', 'CSS'];
+  valor: string;
+
+ constructor(private service: AlertaService){
+
+ }
+
+ enviarMsg():void{
+   this.service.msgAlerta();
+ }
+ 
+ addValor(valor: string): void{
+   this.desenvolvimento.push(valor);
+ }
+
+   valorPassado(valorPassado){
+    this.valor = valorPassado;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,54 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
+import { AlertaService } from "app/alerta.service";
+import { InterpolationBindingComponent } from './interpolation-binding/interpolation-binding.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
+import { NgSwitchCaseComponent } from './ng-switch-case/ng-switch-case.component';
+import { NgForComponent } from './ng-for/ng-for.component';
+import { NgClassComponent } from './ng-class/ng-class.component';
+import { NgStyleComponent } from './ng-style/ng-style.component';
+import { NgContentComponent } from './ng-content/ng-content.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
+import { InputOutputComponent } from './input-output/input-output.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { DiComponent } from './di/di.component';
+import { NomesTecService } from "app/di/nomes-tec.service";
+import { MeuLogService } from "app/di/meu-log.service";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaPessoaComponent,
+    InterpolationBindingComponent,
+    PropertyBindingComponent,
+    TwoWayBindingComponent,
+    NgIfComponent,
+    NgSwitchCaseComponent,
+    NgForComponent,
+    NgClassComponent,
+    NgStyleComponent,
+    NgContentComponent,
+    EventBindingComponent,
+    InputOutputComponent,
+    FormularioComponent,
+    DiComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [AlertaService ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule { 
+
+
+
+}
